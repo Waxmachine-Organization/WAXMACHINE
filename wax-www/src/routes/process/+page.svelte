7f5d2ec76e998004e63d2c5a0e46fe60dcbd1b20
WAXMACHINE <theophilea@ndk.group>
2023-03-22 12:11:54 +0100

    update registration

--- wax-www/src/routes/process/+page.svelte
+++ wax-www/src/routes/process/+page.svelte
@@ -88,7 +88,7 @@
 		right: 0;
 		top: 0;
 		bottom: 0;
-		background-color: aqua;
+		background: url(/images/bg2.jpeg);
 		background-position: center center;
 		background-size: cover;
 		opacity: 0.25;
